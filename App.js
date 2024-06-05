9868b30046022802442b292b206e1d64b10aca86
KevinJair11 <kevinchuquitarco11@gmail.com>
2024-06-05 10:46:30 -0500

    Ejemplo inicial

--- App.js
+++ App.js
@@ -2,18 +2,13 @@ import {
   SafeAreaView,
   StyleSheet,
   Text,
-  TextInput,
-  Button,
-  Alert
 } from "react-native";
-
-import react, { useState } from "react";
 
 export default function App() {
 
   return (
     <SafeAreaView style={styles.container}>
-    
+      <Text>Hola con todo</Text>
     </SafeAreaView>
   );
 }
